f367a38021d95e56fbaf43b4a0e8f4b786008f4c
imsframework@gmail.com <imsframework@gmail.com@c7b0ae48-b0eb-11de-8bdf-3374eb5c7316>
2009-10-06 23:54:55 +0000

    Add project skeleton

---
+++ trunk/doubango/src/sip_dialog_invite.h
@@ -0,0 +1,43 @@
+/*
+	** Copyright (C) 2009 Doubango Telecom <imsframework(at)gmail.com> 
+
+	This file is part of 'Doubango IMS Client Framework'.
+
+    'Doubango IMS Client Framework' is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    'Doubango IMS Client Framework' is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with 'Doubango IMS Client Framework'.  If not, see <http://www.gnu.org/licenses/>.
+
+	** For commercial use, you must purchase a proprietary license from Doubango Telecom.
+*/
+
+/* Copyright (C) 2009 Doubango Telecom
+*
+* Authors:	F0076RTY <imsframework@gmail.com>
+*
+*/
+
+#ifndef __DOUBANGO_DIALOG_INVITE_SM_H__
+#define __DOUBANGO_DIALOG_INVITE_SM_H__
+
+#include <pref.h>
+#include <sip_dialog.h>
+
+PREF_NAMESPACE_START
+
+/* INVITE */
+class sip_dialog_invite : public sip_dialog
+{
+};
+
+PREF_NAMESPACE_END
+
+#endif /* __DOUBANGO_DIALOG_INVITE_SM_H__ */
