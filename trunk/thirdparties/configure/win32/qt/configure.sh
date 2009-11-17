981392fbee07e9ae240a9c319a1f1cf84bac9804
bossiel <bossiel@yahoo.fr>
2009-11-17 23:37:55 +0000

    Remove thirdparties

--- trunk/thirdparties/configure/win32/qt/configure.sh
+++
@@ -1,2 +0,0 @@
-nmake confclean
-configure -static -release -no-exceptions -no-qt3support -no-opengl -no-libtiff -no-dbus -no-phonon -no-phonon-backend -no-webkit -platform win32-msvc2008
--- trunk/thirdparties/lib/win32/libxml/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/usr/local/lib"
-XML2_LIBS="-lxml2     "
-XML2_INCLUDEDIR="-I/usr/local/include/libxml2"
-MODULE_VERSION="xml2-2.7.6"
-
