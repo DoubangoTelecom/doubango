8dfe82fadec1d4e1d3ba47777610a044c2881897
bossiel@yahoo.fr <bossiel@yahoo.fr@c7b0ae48-b0eb-11de-8bdf-3374eb5c7316>
2009-12-05 23:45:54 +0000

    Add s60 headers. Add ragel tools.

---
+++ trunk/thirdparties/s60/configure/libxml/config.h
@@ -0,0 +1,317 @@
+/* config.h.in.  Generated from configure.in by autoheader.  */
+#undef PACKAGE
+#undef VERSION
+#define HAVE_LIBZ
+#define HAVE_LIBM
+#define HAVE_ISINF
+#define HAVE_ISNAN
+#define HAVE_LIBHISTORY
+#define HAVE_LIBREADLINE
+#define HAVE_LIBPTHREAD
+#define HAVE_PTHREAD_H
+
+/* Define if IPV6 support is there */
+#define SUPPORT_IP6
+
+/* Define if getaddrinfo is there */
+#define HAVE_GETADDRINFO
+
+/* Define to 1 if you have the <ansidecl.h> header file. */
+#define HAVE_ANSIDECL_H
+
+/* Define to 1 if you have the <arpa/inet.h> header file. */
+#define HAVE_ARPA_INET_H
+
+/* Define to 1 if you have the <arpa/nameser.h> header file. */
+#define HAVE_ARPA_NAMESER_H
+
+/* Whether struct sockaddr::__ss_family exists */
+#undef HAVE_BROKEN_SS_FAMILY
+
+/* Define to 1 if you have the `class' function. */
+#define HAVE_CLASS
+
+/* Define to 1 if you have the <ctype.h> header file. */
+#define HAVE_CTYPE_H
+
+/* Define to 1 if you have the <dirent.h> header file. */
+#define HAVE_DIRENT_H
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H
+
+/* Have dlopen based dso */
+#define HAVE_DLOPEN
+
+/* Define to 1 if you have the <dl.h> header file. */
+#undef HAVE_DL_H
+
+/* Define to 1 if you have the <errno.h> header file. */
+#define HAVE_ERRNO_H
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H
+
+/* Define to 1 if you have the `finite' function. */
+#define HAVE_FINITE
+
+/* Define to 1 if you have the <float.h> header file. */
+#define HAVE_FLOAT_H
+
+/* Define to 1 if you have the `fpclass' function. */
+#define HAVE_FPCLASS
+
+/* Define to 1 if you have the `fprintf' function. */
+#define HAVE_FPRINTF
+
+/* Define to 1 if you have the `fp_class' function. */
+#define HAVE_FP_CLASS
+
+/* Define to 1 if you have the <fp_class.h> header file. */
+#define HAVE_FP_CLASS_H
+
+/* Define to 1 if you have the `ftime' function. */
+#define HAVE_FTIME
+
+/* Define if getaddrinfo is there */
+#define HAVE_GETADDRINFO
+
+/* Define to 1 if you have the `gettimeofday' function. */
+#define HAVE_GETTIMEOFDAY
+
+/* Define to 1 if you have the <ieeefp.h> header file. */
+#define HAVE_IEEEFP_H
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H
+
+/* Define to 1 if you have the <inttypes.h.h> header file. */
+#define HAVE_INTTYPES_H_H
+
+/* Define if isinf is there */
+#define HAVE_ISINF
+
+/* Define if isnan is there */
+#define HAVE_ISNAN
+
+/* Define to 1 if you have the `isnand' function. */
+#define HAVE_ISNAND
+
+/* Define if history library is there (-lhistory) */
+#define HAVE_LIBHISTORY
+
+/* Define if pthread library is there (-lpthread) */
+#define HAVE_LIBPTHREAD
+
+/* Define if readline library is there (-lreadline) */
+#undef HAVE_LIBREADLINE
+
+/* Have compression library */
+#define HAVE_LIBZ
+
+/* Define to 1 if you have the <limits.h> header file. */
+#define HAVE_LIMITS_H
+
+/* Define to 1 if you have the `localtime' function. */
+#define HAVE_LOCALTIME
+
+/* Define to 1 if you have the <malloc.h> header file. */
+#define HAVE_MALLOC_H
+
+/* Define to 1 if you have the <math.h> header file. */
+#define HAVE_MATH_H
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H
+
+/* Define to 1 if you have the <nan.h> header file. */
+#define HAVE_NAN_H
+
+/* Define to 1 if you have the <ndir.h> header file, and it defines `DIR'. */
+#define HAVE_NDIR_H
+
+/* Define to 1 if you have the <netdb.h> header file. */
+#define HAVE_NETDB_H
+
+/* Define to 1 if you have the <netinet/in.h> header file. */
+#define HAVE_NETINET_IN_H
+
+/* Define to 1 if you have the <poll.h> header file. */
+#undef HAVE_POLL_H
+
+/* Define to 1 if you have the `printf' function. */
+#define HAVE_PRINTF
+
+/* Define if <pthread.h> is there */
+#define HAVE_PTHREAD_H
+
+/* Define to 1 if you have the <resolv.h> header file. */
+#define HAVE_RESOLV_H
+
+/* Have shl_load based dso */
+#undef HAVE_SHLLOAD
+
+/* Define to 1 if you have the `signal' function. */
+#define HAVE_SIGNAL
+
+/* Define to 1 if you have the <signal.h> header file. */
+#define HAVE_SIGNAL_H
+
+/* Define to 1 if you have the `snprintf' function. */
+#define HAVE_SNPRINTF
+
+/* Define to 1 if you have the `sprintf' function. */
+#define HAVE_SPRINTF
+
+/* Define to 1 if you have the `sscanf' function. */
+#define HAVE_SSCANF
+
+/* Define to 1 if you have the `stat' function. */
+#define HAVE_STAT
+
+/* Define to 1 if you have the <stdarg.h> header file. */
+#define HAVE_STDARG_H
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H
+
+/* Define to 1 if you have the `strdup' function. */
+#define HAVE_STRDUP
+
+/* Define to 1 if you have the `strerror' function. */
+#define HAVE_STRERROR
+
+/* Define to 1 if you have the `strftime' function. */
+#define HAVE_STRFTIME
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H
+
+/* Define to 1 if you have the `strndup' function. */
+#define HAVE_STRNDUP
+
+/* Define to 1 if you have the <sys/dir.h> header file, and it defines `DIR'.
+   */
+#define HAVE_SYS_DIR_H
+
+/* Define to 1 if you have the <sys/mman.h> header file. */
+#define HAVE_SYS_MMAN_H
+
+/* Define to 1 if you have the <sys/ndir.h> header file, and it defines `DIR'.
+   */
+#define HAVE_SYS_NDIR_H
+
+/* Define to 1 if you have the <sys/select.h> header file. */
+#define HAVE_SYS_SELECT_H
+
+/* Define to 1 if you have the <sys/socket.h> header file. */
+#define HAVE_SYS_SOCKET_H
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H
+
+/* Define to 1 if you have the <sys/timeb.h> header file. */
+#undef HAVE_SYS_TIMEB_H
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H
+
+/* Define to 1 if you have the <time.h> header file. */
+#define HAVE_TIME_H
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H
+
+/* Whether va_copy() is available */
+#define HAVE_VA_COPY
+
+/* Define to 1 if you have the `vfprintf' function. */
+#define HAVE_VFPRINTF
+
+/* Define to 1 if you have the `vsnprintf' function. */
+#define HAVE_VSNPRINTF
+
+/* Define to 1 if you have the `vsprintf' function. */
+#define HAVE_VSPRINTF
+
+/* Define to 1 if you have the <zlib.h> header file. */
+#define HAVE_ZLIB_H
+
+/* Define to 1 if you have the `_stat' function. */
+#define HAVE__STAT
+
+/* Whether __va_copy() is available */
+#define HAVE___VA_COPY
+
+/* Define as const if the declaration of iconv() needs const. */
+#define ICONV_CONST
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR
+
+/* Name of package */
+#undef PACKAGE
+
+/* Define to the address where bug reports for this package should be sent. */
+#undef PACKAGE_BUGREPORT
+
+/* Define to the full name of this package. */
+#undef PACKAGE_NAME
+
+/* Define to the full name and version of this package. */
+#undef PACKAGE_STRING
+
+/* Define to the one symbol short name of this package. */
+#undef PACKAGE_TARNAME
+
+/* Define to the version of this package. */
+#undef PACKAGE_VERSION
+
+/* Define to 1 if the C compiler supports function prototypes. */
+#undef PROTOTYPES
+
+/* Define to 1 if you have the ANSI C header files. */
+#undef STDC_HEADERS
+
+/* Support for IPv6 */
+#define SUPPORT_IP6
+
+/* Version number of package */
+#define VERSION
+
+/* Determine what socket length (socklen_t) data type is */
+#undef XML_SOCKLEN_T
+
+/* Using the Win32 Socket implementation */
+#undef _WINSOCKAPI_
+
+/* Define like PROTOTYPES; this can be used by system headers. */
+#undef __PROTOTYPES
+
+/* Win32 Std C name mangling work-around */
+#undef snprintf
+
+/* ss_family is not defined here, use __ss_family instead */
+#undef ss_family
+
+/* Win32 Std C name mangling work-around */
+#undef vsnprintf
+
+
+#define _S_ISREG S_ISREG
+
+
+#undef WITH_TRIO
+#define LIBXML_THREAD_ENABLED
+#undef _WIN32
+#undef TRIO_PLATFORM_UNIX
