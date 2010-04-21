f66603b3d0f35bb974ec7a56a06baf43c6aa9068
bossiel <bossiel@yahoo.fr>
2010-04-21 23:44:02 +0000

    Move sip test project Change include paths from absolute to relative.

--- trunk/tinySIP/include/tinysip_config.h
+++ trunk/tinySIP/include/tinysip_config.h
@@ -23,10 +23,6 @@
 #ifndef TINYSIP_CONFIG_H
 #define TINYSIP_CONFIG_H
 
-#if HAVE_CONFIG_H
-	#include "config.h"
-#endif
-
 #ifdef __SYMBIAN32__
 #undef _WIN32 /* Because of WINSCW */
 #endif
@@ -74,6 +70,11 @@
 #include <stdint.h>
 #ifdef __SYMBIAN32__
 #include <stdlib.h>
+#endif
+
+
+#if HAVE_CONFIG_H
+	#include "../config.h"
 #endif
 
 #endif // TINYSIP_CONFIG_H
