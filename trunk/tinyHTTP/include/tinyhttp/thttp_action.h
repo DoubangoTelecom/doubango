8898bf32d5dd981bf9f4ac1d0e8d4b70e4bdb289
bossiel <bossiel@yahoo.fr>
2010-05-04 12:10:10 +0000

    Change include name from lower to upper case.

---
+++ trunk/tinyHTTP/include/tinyhttp/thttp_action.h
@@ -0,0 +1,249 @@
+/*
+* Copyright (C) 2009 Mamadou Diop.
+*
+* Contact: Mamadou Diop <diopmamadou(at)doubango.org>
+*	
+* This file is part of Open Source Doubango Framework.
+*
+* DOUBANGO is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*	
+* DOUBANGO is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*	
+* You should have received a copy of the GNU General Public License
+* along with DOUBANGO.
+*
+*/
+
+/**@file thttp_action.h
+ * @brief HTTP action.
+ *
+ * @author Mamadou Diop <diopmamadou(at)doubango.org>
+ *
+ * @date Created: Sat Nov 8 16:54:58 2009 mdiop
+ */
+#ifndef THTTP_ACTION_H
+#define THTTP_ACTION_H
+
+#include "tinyhttp_config.h"
+
+#include "tinyhttp/thttp_session.h"
+
+#include "tsk_buffer.h"
+#include "tsk_list.h"
+#include "tsk_params.h"
+#include "tsk_options.h"
+
+THTTP_BEGIN_DECLS
+
+typedef uint64_t thttp_action_id_t;			
+#define THTTP_ACTION_INVALID_ID				0
+#define THTTP_ACTION_INVALID_HANDLE			tsk_null
+
+/** List of all supported options.
+* To pass an option to the sesion, use @ref THTTP_ACTION_SET_OPTION() macro.
+*/
+typedef enum thttp_action_option_e
+{
+	THTTP_ACTION_OPTION_TIMEOUT,
+
+}
+thttp_action_option_t;
+
+/** List of actions.
+*/
+typedef enum thttp_action_type_e
+{
+	/* Outgoing GET, PUT, HEAD, DELETE, .... */
+	atype_o_request,
+	/* Incoming message */
+	atype_i_message,
+
+	/* common */
+	atype_closed,
+	atype_close,
+	atype_cancel,
+
+}
+thttp_action_type_t;
+
+typedef enum thttp_action_param_type_e
+{
+	aptype_null = tsk_null,
+
+	aptype_option,
+	aptype_header,
+	aptype_payload,
+}
+thttp_action_param_type_t;
+
+/**@ingroup thttp_action_group
+* @def THTTP_ACTION_SET_OPTION
+* Adds or updates an option. 
+* This is a helper macro for @a thttp_action_*() functions.
+* @param ID_ENUM The id of the option to add/update (@ref thttp_action_option_t).
+* @param VALUE_STR The new value of the parameter (<i>const char*</i>).
+*
+* @code
+thttp_action_GET(session, "http://www.google.com",
+	THTTP_ACTION_SET_PARAM("timeout", "6000"),
+	THTTP_ACTION_SET_NULL());
+* @endcode
+*/
+/**@ingroup thttp_action_group
+* @def THTTP_ACTION_SET_HEADER
+* Adds new HTTP headers to the request.
+* This is a helper macro for @a thttp_action_*() functions.
+* @param NAME_STR The name of the header (<i>const char*</i>).
+* @param VALUE_STR The value of the header (<i>const char*</i>). Should not contains the trailing CRLF.
+*
+* @code
+thttp_action_GET(session, "http://www.doubango.org"
+	THTTP_ACTION_SET_HEADER("Pragma", "No-Cache"),
+	THTTP_ACTION_SET_HEADER("Connection", "Keep-Alive"),
+	THTTP_ACTION_SET_NULL());
+* @endcode
+*/
+/**@ingroup thttp_action_group
+* @def THTTP_ACTION_SET_PAYLOAD
+* Adds a content (or payload) to the request. You should also add a content-type header by using 
+* @ref THTTP_ACTION_SET_HEADER() macro. You should not add the content-length header.
+* This is a helper macro for @a thttp_action_*() functions.
+* @param PAY_PTR A pointer to the payload (<i>const void*</i>).
+* @param PAY_SIZE The size of the payload (<i>size_t</i>).
+*
+* @code
+thttp_action_PUT(session, "http://www.doubango.org"
+	THTTP_ACTION_SET_HEADER("Pragma", "No-Cache"),
+	THTTP_ACTION_SET_HEADER("Connection", "Keep-Alive"),
+	THTTP_ACTION_SET_HEADER("Content-length", "application/mytype"),
+	
+	THTTP_ACTION_SET_PAYLOAD("Salut", 5),
+
+	THTTP_ACTION_SET_NULL());
+* @endcode
+*/
+/**@ingroup thttp_action_group
+* @def THTTP_ACTION_SET_NULL
+* Ends action parameters. Must always be the last one.
+*/
+#define THTTP_ACTION_SET_OPTION(ID_ENUM, VALUE_STR)			aptype_option, (thttp_action_option_t)ID_ENUM, (const char*)VALUE_STR
+#define THTTP_ACTION_SET_HEADER(NAME_STR, VALUE_STR)		aptype_header, (const char*)NAME_STR, (const char*)VALUE_STR
+#define THTTP_ACTION_SET_PAYLOAD(PAY_PTR, PAY_SIZE)			aptype_payload, (const void*)PAY_PTR, (size_t)PAY_SIZE
+#define THTTP_ACTION_SET_NULL()								aptype_null
+
+typedef struct thttp_action_s
+{
+	TSK_DECLARE_OBJECT;
+	
+	thttp_action_type_t type;
+	const char* url;
+	const char* method;
+
+	tsk_options_L_t *options;
+	tsk_params_L_t *headers;
+	tsk_buffer_t* payload;
+}
+thttp_action_t;
+
+typedef void thttp_action_handle_t;
+
+/**@ingroup thttp_action_group
+* @def thttp_action_CONNECT
+* Sends @a CONNECT method request. This function is non-blocking and the result will be posted to the callback function.
+* @param session The @a session (or connection) to use.
+* @param urlstring The Request-URI of the request.
+* @param ... Any @b THTTP_ACTION_SET_*() macros. MUST ends with @ref THTTP_ACTION_SET_NULL().
+* @retval Zero if succeed and non-zero error code otherwise.
+*/
+/**@ingroup thttp_action_group
+* @def thttp_action_DELETE
+* Sends @a DELETE method request. This function is non-blocking and the result will be posted to the callback function.
+* @param session The @a session (or connection) to use.
+* @param urlstring The Request-URI of the request.
+* @param ... Any @b THTTP_ACTION_SET_*() macros. MUST ends with @ref THTTP_ACTION_SET_NULL().
+* @retval Zero if succeed and non-zero error code otherwise.
+*/
+/**@ingroup thttp_action_group
+* @def thttp_action_GET
+* Sends @a GET method request. This function is non-blocking and the result will be posted to the callback function.
+* @param session The @a session (or connection) to use.
+* @param urlstring The Request-URI of the request.
+* @param ... Any @b THTTP_ACTION_SET_*() macros. MUST ends with @ref THTTP_ACTION_SET_NULL().
+* @retval Zero if succeed and non-zero error code otherwise.
+*/
+/**@ingroup thttp_action_group
+* @def thttp_action_HEAD
+* Sends @a HEAD method request. This function is non-blocking and the result will be posted to the callback function.
+
+* @param session The @a session (or connection) to use.
+* @param urlstring The Request-URI of the request.
+* @param ... Any @b THTTP_ACTION_SET_*() macros. MUST ends with @ref THTTP_ACTION_SET_NULL().
+* @retval Zero if succeed and non-zero error code otherwise.
+*/
+/**@ingroup thttp_action_group
+* @def thttp_action_OPTIONS
+* Sends @a OPTIONS method request. This function is non-blocking and the result will be posted to the callback function.
+* @param session The @a session (or connection) to use.
+* @param urlstring The Request-URI of the request.
+* @param ... Any @b THTTP_ACTION_SET_*() macros. MUST ends with @ref THTTP_ACTION_SET_NULL().
+* @retval Zero if succeed and non-zero error code otherwise.
+*/
+/**@ingroup thttp_action_group
+* @def thttp_action_PATCH
+* Sends @a PATCH method request. This function is non-blocking and the result will be posted to the callback function.
+
+* @param session The @a session (or connection) to use.
+* @param urlstring The Request-URI of the request.
+* @param ... Any @b THTTP_ACTION_SET_*() macros. MUST ends with @ref THTTP_ACTION_SET_NULL().
+* @retval Zero if succeed and non-zero error code otherwise.
+*/
+/**@ingroup thttp_action_group
+* @def thttp_action_POST
+* Sends @a POST method request. This function is non-blocking and the result will be posted to the callback function.
+* @param session The @a session (or connection) to use.
+* @param urlstring The Request-URI of the request.
+* @param ... Any @b THTTP_ACTION_SET_*() macros. MUST ends with @ref THTTP_ACTION_SET_NULL().
+* @retval Zero if succeed and non-zero error code otherwise.
+*/
+/**@ingroup thttp_action_group
+* @def thttp_action_PUT
+* Sends @a PUT method request. This function is non-blocking and the result will be posted to the callback function.
+* @param session The @a session (or connection) to use.
+* @param urlstring The Request-URI of the request.
+* @param ... Any @b THTTP_ACTION_SET_*() macros. MUST ends with @ref THTTP_ACTION_SET_NULL().
+* @retval Zero if succeed and non-zero error code otherwise.
+*/
+/**@ingroup thttp_action_group
+* @def thttp_action_TRACE
+* Sends @a TRACE method request. This function is non-blocking and the result will be posted to the callback function.
+* @param session The @a session (or connection) to use.
+* @param urlstring The Request-URI of the request.
+* @param ... Any @b THTTP_ACTION_SET_*() macros. MUST ends with @ref THTTP_ACTION_SET_NULL().
+* @retval Zero if succeed and non-zero error code otherwise.
+*/
+TINYHTTP_API int thttp_action_perform(thttp_session_handle_t *session, const char* urlstring, const char* method, ...);
+#define thttp_action_CONNECT(session, urlstring, ...) thttp_action_perform(session, urlstring, "CONNECT", __VA_ARGS__)
+#define thttp_action_DELETE(session, urlstring, ...) thttp_action_perform(session, urlstring, "DELETE", __VA_ARGS__)
+#define thttp_action_GET(session, urlstring, ...) thttp_action_perform(session, urlstring, "GET", __VA_ARGS__)
+#define thttp_action_HEAD(session, urlstring, ...) thttp_action_perform(session, urlstring, "HEAD", __VA_ARGS__)
+#define thttp_action_OPTIONS(session, urlstring, ...) thttp_action_perform(session, urlstring, "OPTIONS", __VA_ARGS__)
+#define thttp_action_PATCH(session, urlstring, ...) thttp_action_perform(session, urlstring, "PATCH", __VA_ARGS__)
+#define thttp_action_POST(session, urlstring, ...) thttp_action_perform(session, urlstring, "POST", __VA_ARGS__)
+#define thttp_action_PUT(session, urlstring, ...) thttp_action_perform(session, urlstring, "PUT", __VA_ARGS__)
+#define thttp_action_TRACE(session, urlstring, ...) thttp_action_perform(session, urlstring, "TRACE", __VA_ARGS__)
+
+TINYHTTP_API thttp_action_t* thttp_action_create(thttp_action_type_t type, const char* urlstring, const char* method, va_list* app);
+
+typedef tsk_list_t thttp_actions_L_t; /**< List of @ref thttp_action_handle_t elements. */
+TINYHTTP_GEXTERN const tsk_object_def_t *thttp_action_def_t;
+
+THTTP_END_DECLS
+
+#endif /* THTTP_ACTION_H */
+
---
+++ trunk/tinyHTTP/include/tinyhttp/thttp_session.h
@@ -0,0 +1,206 @@
+/*
+* Copyright (C) 2009 Mamadou Diop.
+*
+* Contact: Mamadou Diop <diopmamadou(at)doubango.org>
+*	
+* This file is part of Open Source Doubango Framework.
+*
+* DOUBANGO is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*	
+* DOUBANGO is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*	
+* You should have received a copy of the GNU General Public License
+* along with DOUBANGO.
+*
+*/
+
+/**@file thttp_session.h
+ * @brief HTTP session.
+ *
+ * @author Mamadou Diop <diopmamadou(at)doubango.org>
+ *
+ * @date Created: Sat Nov 8 16:54:58 2009 mdiop
+ */
+#ifndef THTTP_SESSION_H
+#define THTTP_SESSION_H
+
+#include "tinyhttp_config.h"
+
+#include "tinyhttp/auth/thttp_challenge.h"
+#include "tinyhttp/thttp_message.h"
+
+#include "tinyhttp/thttp_dialog.h"
+
+#include "tnet_types.h"
+
+#include "tsk_object.h"
+#include "tsk_list.h"
+#include "tsk_params.h"
+#include "tsk_options.h"
+
+THTTP_BEGIN_DECLS
+
+//FD
+struct thttp_message_s;
+
+typedef uint64_t thttp_session_id_t;			
+#define THTTP_SESSION_INVALID_ID				0
+#define THTTP_SESSION_INVALID_HANDLE			tsk_null
+
+/** List of all supported options.
+* To pass an option to the sesion, use @ref THTTP_SESSION_SET_OPTION() macro.
+*/
+typedef enum thttp_session_option_e
+{
+	THTTP_SESSION_OPTION_TIMEOUT,
+
+}
+thttp_session_option_t;
+
+typedef enum thttp_session_param_type_e
+{
+	httpp_null = tsk_null,
+
+	httpp_option,
+	httpp_cred,
+	httpp_header,
+	httpp_context,
+}
+thttp_session_param_type_t;
+
+/**@ingroup thttp_session_group
+* @def THTTP_SESSION_SET_OPTION
+* Adds or updates an option. 
+* This is a helper macro for @ref thttp_session_create and @ref thttp_session_set.
+* @param ID_ENUM The id of the option to add/update (@ref thttp_session_option_t).
+* @param VALUE_STR The new value of the option (<i>const char*</i>).
+*
+* @code
+// session = thttp_session_create(stack,
+thttp_session_set(session,
+	THTTP_SESSION_SET_PARAM(THTTP_SESSION_OPTION_TIMEOUT, "6000"),
+	THTTP_SESSION_SET_NULL());
+* @endcode
+*/
+/**@ingroup thttp_session_group
+* @def THTTP_SESSION_SET_CRED
+* Sets the user's creadentials.
+* This is a helper macro for @ref thttp_session_create and @ref thttp_session_set.
+* @param USERNAME_STR The username (const char*).
+* @param PASSWORD_STR The password(const char*).
+*
+* @code
+// session = thttp_session_create(stack,
+thttp_session_set(session,
+	THTTP_SESSION_SET_CRED("ali baba", "open sesame"),
+	THTTP_SESSION_SET_NULL());
+* @endcode
+*/
+/**@ingroup thttp_session_group
+* @def THTTP_SESSION_SET_HEADER
+* Adds new HTTP headers to the session. The value of the header will be updated if it already exist.
+* This is a helper macro for @ref thttp_session_create and @ref thttp_session_set.
+* @param NAME_STR The name of the header (<i>const char*</i>) to add or update.
+* @param VALUE_STR The value of the header (<i>const char*</i>). Should not contains the trailing CRLF.
+* @sa @ref THTTP_SESSION_UNSET_HEADER
+*
+* @code
+// session = thttp_session_create(stack,
+thttp_session_set(session,
+	THTTP_SESSION_SET_HEADER("Pragma", "No-Cache"),
+	THTTP_SESSION_SET_HEADER("Connection", "Keep-Alive"),
+	THTTP_SESSION_SET_NULL());
+* @endcode
+*/
+/**@ingroup thttp_session_group
+* @def THTTP_SESSION_UNSET_HEADER
+* Removes a header. This header should be previously added by using @ref THTTP_SESSION_SET_HEADER().
+* This is a helper macro for @ref thttp_session_create and @ref thttp_session_set.
+* @param NAME_STR The name of the header (<i>const char*</i>) to remove.
+* @sa @ref THTTP_SESSION_SET_HEADER
+*
+* @code
+// session = thttp_session_create(stack,
+thttp_session_set(session,
+	THTTP_SESSION_UNSET_HEADER("Pragma"),
+	THTTP_SESSION_UNSET_HEADER("Connection"),
+	THTTP_SESSION_SET_NULL());
+* @endcode
+*/
+/**@ingroup thttp_session_group
+* @def THTTP_SESSION_SET_CONTEXT
+* Sets user data (context). Will be return to the application layer each time the callback function is called.
+* This is a helper macro for @ref thttp_session_create and @ref thttp_session_set.
+* @param CTX_PTR A pointer to the data(const void*).
+*
+* @code
+// session = thttp_session_create(stack,
+thttp_session_set(session,
+	THTTP_SESSION_SET_CONTEXT(ctx),
+	THTTP_SESSION_SET_NULL());
+* @endcode
+*/
+/**@ingroup thttp_session_group
+* @def THTTP_SESSION_SET_NULL
+* Ends session parameters. Must always be the last one.
+*/
+#define THTTP_SESSION_SET_OPTION(ID_ENUM, VALUE_STR)			httpp_option, (thttp_session_option_t)ID_ENUM, (const char*)VALUE_STR
+#define THTTP_SESSION_SET_CRED(USERNAME_STR, PASSWORD_STR)		httpp_cred, (const char*)USERNAME_STR, (const char*)PASSWORD_STR
+#define THTTP_SESSION_SET_HEADER(NAME_STR, VALUE_STR)			httpp_header, (const char*)NAME_STR, (const char*)VALUE_STR
+#define THTTP_SESSION_UNSET_HEADER(NAME_STR)					THTTP_SESSION_SET_HEADER(NAME_STR, (const char*)-1)
+#define THTTP_SESSION_SET_CONTEXT(CTX_PTR)						httpp_context, (const void*)CTX_PTR
+#define THTTP_SESSION_SET_NULL()								httpp_null
+
+typedef struct thttp_session_s
+{
+	TSK_DECLARE_OBJECT;
+
+	thttp_session_id_t id;
+	const struct thttp_stack_s* stack;
+	const void* context; // user's context
+	tsk_options_L_t *options;
+	tsk_params_L_t *headers;
+
+	tnet_fd_t fd;
+
+	thttp_challenges_L_t *challenges;
+	thttp_dialogs_L_t* dialogs;
+
+	struct{
+		char* usename;
+		char* password;
+	}cred;
+
+	TSK_DECLARE_SAFEOBJ;
+}
+thttp_session_t;
+
+typedef tsk_list_t thttp_sessions_L_t; /**< List of @ref thttp_session_handle_t elements. */
+
+/** Pointer to a HTTP/HTTPS session. */
+typedef void thttp_session_handle_t;
+/** Pointer to a HTTP/HTTPS stack object. */
+typedef void thttp_stack_handle_t;
+
+TINYHTTP_API thttp_session_handle_t* thttp_session_create(const thttp_stack_handle_t* stack, ...);
+TINYHTTP_API int thttp_session_set(thttp_session_handle_t *self, ...);
+TINYHTTP_API thttp_session_id_t thttp_session_get_id(const thttp_session_handle_t *self);
+TINYHTTP_API const void* thttp_session_get_context(const thttp_session_handle_t *self);
+
+int thttp_session_update_challenges(thttp_session_t *self, const thttp_response_t* response, tsk_bool_t answered);
+int thttp_session_signal_closed(thttp_session_t *self);
+thttp_session_t* thttp_session_get_by_fd(thttp_sessions_L_t* sessions, tnet_fd_t fd);
+
+
+TINYHTTP_GEXTERN const tsk_object_def_t *thttp_session_def_t;
+
+THTTP_END_DECLS
+
+#endif /* THTTP_SESSION_H */
+
