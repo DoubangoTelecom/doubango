63177f7b448dfe9cb448af1ae770ece0d1d6b09a
bossiel@yahoo.fr <bossiel@yahoo.fr@c7b0ae48-b0eb-11de-8bdf-3374eb5c7316>
2010-05-04 12:10:10 +0000

    Change include name from lower to upper case.

---
+++ trunk/tinyHTTP/include/tinyhttp/parsers/thttp_parser_message.h
@@ -0,0 +1,44 @@
+/*
+* Copyright (C) 2009 Mamadou Diop.
+*
+* Contact: Mamadou Diop <diopmamadou(at)doubango.org>
+*	
+* This file is part of Open Source Doubango Framework.
+*
+* DOUBANGO is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*	
+* DOUBANGO is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*	
+* You should have received a copy of the GNU General Public License
+* along with DOUBANGO.
+*
+*/
+
+/**@file thttp_parser_message.h
+ * @brief HTTP message parser.
+ *
+ * @author Mamadou Diop <diopmamadou(at)doubango.org>
+ *
+ * @date Created: Sat Nov 8 16:54:58 2009 mdiop
+ */
+#ifndef TINYHTTP_PARSER_MESSAGE_H
+#define TINYHTTP_PARSER_MESSAGE_H
+
+#include "tinyhttp_config.h"
+#include "tinyhttp/thttp_message.h"
+#include "tsk_ragel_state.h"
+
+THTTP_BEGIN_DECLS
+
+TINYHTTP_API int thttp_message_parse(tsk_ragel_state_t *state, thttp_message_t **result, tsk_bool_t extract_content);
+
+THTTP_END_DECLS
+
+#endif /* TINYHTTP_PARSER_MESSAGE_H */
+
