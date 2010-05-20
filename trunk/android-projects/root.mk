22b9a829c1e60ab2e1f185c2e6e0ccbecd7ac897
bossiel@yahoo.fr <bossiel@yahoo.fr@c7b0ae48-b0eb-11de-8bdf-3374eb5c7316>
2010-05-20 15:09:33 +0000

    Update droid makefiles.

--- trunk/android-projects/root.mk
+++ trunk/android-projects/root.mk
@@ -2,9 +2,9 @@
 ##	Root configuration file shared by all android projects.
 ######
 
-export ANDROID_NDK_ROOT=/cygdrive/c/android-ndk-r3
+export ANDROID_NDK_ROOT=/cygdrive/c/android-ndk
 export ANDROID_SDK_ROOT=/cygdrive/c/android-sdk
-export ANDROID_PLATFORM=$(ANDROID_NDK_ROOT)/build/platforms/android-5
+export ANDROID_PLATFORM=$(ANDROID_NDK_ROOT)/build/platforms/android-4
 
 # Output directory
 export OUTPUT_DIR=$(shell pwd)/output
@@ -19,42 +19,45 @@ export AR=arm-eabi-ar
 export CC=arm-eabi-gcc
 export CPP=arm-eabi-g++
 
-export CFLAGS+=$(DEBUG_FLAGS) -I$(ANDROID_PLATFORM)/arch-arm/usr/include \
+export CFLAGS_COMMON=$(CFLAGS) $(DEBUG_FLAGS) -I$(ANDROID_PLATFORM)/arch-arm/usr/include \
 -march=armv5te \
 -mtune=xscale \
 -msoft-float \
+-D__ARM_ARCH_5__ \
+-D__ARM_ARCH_5T__ \
+-D__ARM_ARCH_5E__ \
+-D__ARM_ARCH_5TE__ \
+-MMD \
+-MP \
+-DANDROID
+
+export CFLAGS_LIB= $(CFLAGS_COMMON) \
 -fpic \
 -mthumb-interwork \
 -ffunction-sections \
 -funwind-tables \
 -fstack-protector \
 -fno-short-enums \
--D__ARM_ARCH_5__ \
--D__ARM_ARCH_5T__ \
--D__ARM_ARCH_5E__ \
--D__ARM_ARCH_5TE__ \
 -mthumb \
 -Os \
 -fomit-frame-pointer \
 -fno-strict-aliasing \
--finline-limit=64 \
--DANDROID \
--MMD \
--MP 
+-finline-limit=64
 
-export LDFLAGS_COMMON=-Wl,-rpath=/system/lib,-rpath-link=$(ANDROID_PLATFORM)/arch-arm/usr/lib,-rpath-link=$(OUTPUT_DIR),-dynamic-linker=/system/bin/linker -L$(ANDROID_PLATFORM)/arch-arm/usr/lib
+
+export LDFLAGS_COMMON=$(LDFLAGS) -Wl,-rpath=/system/lib,-rpath-link=$(ANDROID_PLATFORM)/arch-arm/usr/lib,-rpath-link=$(OUTPUT_DIR),-dynamic-linker=/system/bin/linker -L$(ANDROID_PLATFORM)/arch-arm/usr/lib
 export LDFLAGS_COMMON+=-nostdlib -lc -L$(OUTPUT_DIR)
 
 ifeq ($(BT), static)
 	export EXT=a
-	export LDFLAGS=
+	export LDFLAGS_LIB=
 	
-	export CFLAGS+=-static
+	export CFLAGS_LIB+=-static
 else
 	export EXT=so
 	
-	export LDFLAGS=$(LDFLAGS_COMMON)
-	export LDFLAGS+=-Wl,--no-undefined
-	export LDFLAGS+=-Wl,--no-whole-archive
-	export LDFLAGS+=-Wl,-soname,lib$(PROJECT).$(EXT),-Bsymbolic,-shared,--whole-archive
+	export LDFLAGS_LIB=$(LDFLAGS_COMMON)
+	export LDFLAGS_LIB+=-Wl,--no-undefined
+	export LDFLAGS_LIB+=-Wl,--no-whole-archive
+	export LDFLAGS_LIB+=-Wl,-soname,lib$(PROJECT).$(EXT),-Bsymbolic,-shared,--whole-archive
 endif
