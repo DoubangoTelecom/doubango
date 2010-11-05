8cdaf31d6558fb6254b6531ec216e0e9c4032156
bossiel <bossiel@yahoo.fr>
2010-11-05 06:59:27 +0000

    Update script

--- trunk/android-projects/buildAll.sh
+++ trunk/android-projects/buildAll.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Build tinyDEMO for Google Android Systems
 
-for project in tinySAK tinyNET tinyIPSec tinySMS tinySIGCOMP tinyHTTP tinySDP tinyRTP tinyMEDIA tinySIP tinyDAV tinyDEMO
+for project in tinySAK tinyNET tinyHTTP tinyXCAP tinyIPSec tinySMS tinySIGCOMP tinySDP tinyRTP tinyMSRP tinyMEDIA tinyDAV tinySIP
 do
 	echo -e building "$project....\n"
 	make PROJECT=$project clean
