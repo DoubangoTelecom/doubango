219b05e313e68da14ec7a5302e5f119b3a36d933
bossiel <bossiel@yahoo.fr>
2011-01-11 16:15:13 +0000

    Use same ssrc for dtmf as audio session.

--- trunk/tinyDAV/src/audio/tdav_session_audio.c
+++ trunk/tinyDAV/src/audio/tdav_session_audio.c
@@ -697,7 +697,7 @@ tdav_session_audio_dtmfe_t* _tdav_session_audio_dtmfe_create(const tdav_session_
 	}
 	dtmfe->session = session;
 
-	if(!(dtmfe->packet = trtp_rtp_packet_create(ssrc, seq, timestamp, format, M))){
+	if(!(dtmfe->packet = trtp_rtp_packet_create((session && session->rtp_manager) ? session->rtp_manager->rtp.ssrc : ssrc, seq, timestamp, format, M))){
 		TSK_DEBUG_ERROR("Failed to create DTMF RTP packet");
 		TSK_OBJECT_SAFE_FREE(dtmfe);
 		return tsk_null;
