a5d278b4e17a35b39a8cf63865ae94d4ed45f9ec
bossiel <bossiel@yahoo.fr>
2011-04-29 18:42:14 +0000

    Update minimal build script for android

--- branches/2.0/doubango/bindings/java/android/minimal.sh
+++ branches/2.0/doubango/bindings/java/android/minimal.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Build tinyWRAP for Google Android Systems
 
-# wiPhone parameters
-export WIPHONE_PARAMS="FFMPEG=no H264=no THEORA=no OPENCORE_AMR=no SPEEX_DSP=yes SPEEX_JB=yes SPEEX_RESAMPLER=yes SPEEX_DENOISE=no SPEEX=no ILBC=no LIBGSM=no G729=no"
+# Minimal parameters
+export MINIMAL_PARAMS="FFMPEG=no H264=no THEORA=no OPENCORE_AMR=no SPEEX_DSP=yes SPEEX_JB=yes SPEEX_RESAMPLER=yes SPEEX_DENOISE=no SPEEX=no ILBC=no LIBGSM=no G729=no"
 export _BT=static
 
 for IS_NEON in no yes
@@ -10,12 +10,12 @@ do
 	for project in tinySAK tinyNET tinyHTTP tinyXCAP tinyIPSec tinySMS tinySIGCOMP tinySDP tinyRTP tinyMSRP tinyMEDIA tinyDAV tinySIP
 	#for project in tinyDAV
 	do
-		echo -e building "$project with parameters: $@ NEON=$IS_NEON $WIPHONE_PARAMS\n"
+		echo -e building "$project with parameters: $@ NEON=$IS_NEON $MINIMAL_PARAMS\n"
 		make PROJECT=$project clean
-		make PROJECT=$project BT=$_BT $@ NEON=$IS_NEON $WIPHONE_PARAMS all
+		make PROJECT=$project BT=$_BT $@ NEON=$IS_NEON $MINIMAL_PARAMS all
 	done
 
 	echo building tinyWRAP....
 	make PROJECT=tinyWRAP clean
-	make PROJECT=tinyWRAP BT=shared LT=$_BT $@ NEON=$IS_NEON $WIPHONE_PARAMS all
+	make PROJECT=tinyWRAP BT=shared LT=$_BT $@ NEON=$IS_NEON $MINIMAL_PARAMS all
 done
