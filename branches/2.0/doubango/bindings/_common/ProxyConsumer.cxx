1784119510a7981869c1656318f01579e31ff21d
bossiel <bossiel@yahoo.fr>
2013-09-12 11:20:09 +0000

    Add error message on proxy plugin

--- branches/2.0/doubango/bindings/_common/ProxyConsumer.cxx
+++ branches/2.0/doubango/bindings/_common/ProxyConsumer.cxx
@@ -79,6 +79,9 @@ int twrap_consumer_proxy_audio_prepare(tmedia_consumer_t* self, const tmedia_cod
 			}
 		}
 	}
+	else {
+		TSK_DEBUG_ERROR("Invalid parameter/state: codec=%d, manager=%d", codec, manager);
+	}
 	
 	return ret;
 }
