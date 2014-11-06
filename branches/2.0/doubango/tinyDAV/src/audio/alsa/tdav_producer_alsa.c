74357497cecb58d3f2d73c54f3411e4b996d320d
bossiel@yahoo.fr <bossiel@yahoo.fr@c7b0ae48-b0eb-11de-8bdf-3374eb5c7316>
2014-11-06 04:58:49 +0000

    Update ALSA implementation

--- branches/2.0/doubango/tinyDAV/src/audio/alsa/tdav_producer_alsa.c
+++ branches/2.0/doubango/tinyDAV/src/audio/alsa/tdav_producer_alsa.c
@@ -52,7 +52,7 @@ static void* TSK_STDCALL _tdav_producer_alsa_record_thread(void *param)
 	while (p_alsa->b_started) {
 		tdav_common_alsa_lock(&p_alsa->alsa_common);
 		if ((err = snd_pcm_readi(p_alsa->alsa_common.p_handle, p_alsa->alsa_common.p_buff_ptr, p_alsa->alsa_common.n_buff_size_in_samples)) != p_alsa->alsa_common.n_buff_size_in_samples) {
-			ALSA_DEBUG_ERROR ("Failed to read data from audio interface failed (%s)", snd_strerror(err));
+			ALSA_DEBUG_ERROR ("Failed to read data from audio interface failed (%d->%s)", err, snd_strerror(err));
 			tdav_common_alsa_unlock(&p_alsa->alsa_common);
 			goto bail;
 		}
@@ -89,12 +89,15 @@ static int tdav_producer_alsa_prepare(tmedia_producer_t* self, const tmedia_code
 {
 	tdav_producer_alsa_t*  p_alsa = (tdav_producer_alsa_t*)self;
 	int err = 0;
+	ALSA_DEBUG_INFO("******* tdav_producer_alsa_prepare ******");
 
 	if (! p_alsa || !codec && codec->plugin) {
 		ALSA_DEBUG_ERROR("Invalid parameter");
 		return -1;
 	}
-	
+
+	tdav_common_alsa_lock(&p_alsa->alsa_common);
+
 	// Set using requested
 	TMEDIA_PRODUCER( p_alsa)->audio.channels = TMEDIA_CODEC_CHANNELS_AUDIO_ENCODING(codec);
 	TMEDIA_PRODUCER( p_alsa)->audio.rate = TMEDIA_CODEC_RATE_ENCODING(codec);
@@ -103,7 +106,7 @@ static int tdav_producer_alsa_prepare(tmedia_producer_t* self, const tmedia_code
 	// Prepare
 	err = tdav_common_alsa_prepare(&p_alsa->alsa_common, tsk_true/*is_capture*/, TMEDIA_PRODUCER( p_alsa)->audio.ptime, TMEDIA_PRODUCER( p_alsa)->audio.channels, TMEDIA_PRODUCER( p_alsa)->audio.rate);
 	if (err) {
-		return err;
+		goto bail;
 	}
 	
 	ALSA_DEBUG_INFO("prepared: req_channels=%d; req_rate=%d, resp_channels=%d; resp_rate=%d",
@@ -114,6 +117,8 @@ static int tdav_producer_alsa_prepare(tmedia_producer_t* self, const tmedia_code
 	TMEDIA_PRODUCER(p_alsa)->audio.channels = p_alsa->alsa_common.channels;
 	TMEDIA_PRODUCER(p_alsa)->audio.rate = p_alsa->alsa_common.sample_rate;
 	
+bail:
+	tdav_common_alsa_unlock(&p_alsa->alsa_common);
 	return err;
 }
 
@@ -122,23 +127,34 @@ static int tdav_producer_alsa_start(tmedia_producer_t* self)
 	tdav_producer_alsa_t* p_alsa = (tdav_producer_alsa_t*)self;
 	int err = 0;
 
-	if (! p_alsa) {
+	ALSA_DEBUG_INFO("******* tdav_producer_alsa_start ******");
+
+	if (!p_alsa) {
 		ALSA_DEBUG_ERROR("Invalid parameter");
 		return -1;
+	}
+	
+	tdav_common_alsa_lock(&p_alsa->alsa_common);
+
+	if (p_alsa->b_started) {
+		ALSA_DEBUG_WARN("Already started");
+		goto bail;
 	}
 
 	/* start device */
 	err = tdav_common_alsa_start(&p_alsa->alsa_common);
 	if (err) {
-		return err;
+		goto bail;
 	}
 
 	 /* start thread */
 	 p_alsa->b_started = tsk_true;
-	 tsk_thread_create(& p_alsa->tid[0], _tdav_producer_alsa_record_thread,  p_alsa);
+	 tsk_thread_create(&p_alsa->tid[0], _tdav_producer_alsa_record_thread,  p_alsa);
 
 	ALSA_DEBUG_INFO("started");
 
+bail:
+	tdav_common_alsa_unlock(&p_alsa->alsa_common);
 	return err;
 }
 
@@ -165,11 +181,11 @@ static int tdav_producer_alsa_stop(tmedia_producer_t* self)
 		ALSA_DEBUG_ERROR("Invalid parameter");
 		return -1;
 	}
-
-	err = tdav_common_alsa_stop(&p_alsa->alsa_common);
-
+	
 	/* should be done here */
 	p_alsa->b_started = tsk_false;
+
+	err = tdav_common_alsa_stop(&p_alsa->alsa_common);
 
 	/* stop thread */
 	if (p_alsa->tid[0]) {
@@ -190,6 +206,7 @@ static tsk_object_t* tdav_producer_alsa_ctor(tsk_object_t * self, va_list * app)
 {
 	tdav_producer_alsa_t *p_alsa = (tdav_producer_alsa_t*)self;
 	if (p_alsa) {
+		ALSA_DEBUG_INFO("create");
 		/* init base */
 		tdav_producer_audio_init(TDAV_PRODUCER_AUDIO(p_alsa));
 		/* init self */
@@ -231,7 +248,7 @@ static const tmedia_producer_plugin_def_t tdav_producer_alsa_plugin_def_s =
 	&tdav_producer_alsa_def_s,
 	
 	tmedia_audio,
-	"Microsoft alsa producer",
+	"Linux ALSA producer",
 	
 	tdav_producer_alsa_set,
 	tdav_producer_alsa_prepare,
